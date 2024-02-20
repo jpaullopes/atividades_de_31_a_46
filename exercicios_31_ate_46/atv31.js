601e759c5381921ac478be61a746fe04368e89a1
jão <159722770+jpaullopes@users.noreply.github.com>
2024-02-19 21:09:59 -0300

    atv31.js

--- exercicios_31_ate_46/atv31.js
+++ exercicios_31_ate_46/atv31.js
@@ -4,11 +4,13 @@ console.log('CONVERSOR DE BINÁRIO PARA DECIMAL')
 //entrada
 const numero_binario = question('Digite o número em binário[4 Digítos]: ')
 //processamento
-const numero_bin_separado = numero_binario.split('')
-const numero_bin1 = Number.parseInt(numero_bin_separado[3]) * 1
-const numero_bin2 = Number.parseInt(numero_bin_separado[2]) * 2
-const numero_bin3 = Number.parseInt(numero_bin_separado[1]) * 4
-const numero_bin4 = Number.parseInt(numero_bin_separado[0]) * 8
+const numero_bin1 = Number.parseInt(numero_binario / 1000) * 8
+let resto = numero_binario % 1000
+const numero_bin2 = Number.parseInt(resto / 100) * 4
+resto = resto % 100
+const numero_bin3 = Number.parseInt(resto / 10) * 2
+const numero_bin4 = (resto % 10) * 1
 const numero_decimal = numero_bin1 + numero_bin2 + numero_bin3 + numero_bin4
 //saída
 console.log(`O numero binario ${numero_binario} em decimal corresponde ao número ${numero_decimal}.`)
+
