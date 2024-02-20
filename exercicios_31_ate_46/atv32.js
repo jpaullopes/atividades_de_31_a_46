871e2d830b33af0c6b181faf5de7a59dbbc96cdb
jão <159722770+jpaullopes@users.noreply.github.com>
2024-02-19 21:16:12 -0300

    atv32.js

--- exercicios_31_ate_46/atv32.js
+++ exercicios_31_ate_46/atv32.js
@@ -3,9 +3,11 @@ import { question } from "readline-sync"
 //entrada
 const numero_inteiro = Number.parseInt(question('Digite um número inteiro[3 dígitos]: '))
 //processamento
-var numero_inverso = String(numero_inteiro).split('')
-numero_inverso.reverse()
-numero_inverso = Number(numero_inverso.join(''))
+const centena = Number.parseInt(numero_inteiro / 100)
+const resto = numero_inteiro % 100
+const dezena = Number.parseInt(resto / 10)
+const unidade = resto % 10
+const numero_inverso = unidade * 100 + dezena * 10 + centena
 const diferença_dos_numeros = Math.abs( numero_inteiro - numero_inverso)
 //saída
 console.log(`A difeença entre ${numero_inteiro} e ${numero_inverso} é igual a: ${diferença_dos_numeros}.`)
