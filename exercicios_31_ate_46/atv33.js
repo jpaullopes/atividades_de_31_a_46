7325117c0b8eed480833a203931a6b2086cd3121
jão <159722770+jpaullopes@users.noreply.github.com>
2024-02-19 21:18:03 -0300

    atv33.js

--- exercicios_31_ate_46/atv33.js
+++ exercicios_31_ate_46/atv33.js
@@ -4,9 +4,11 @@ import { question } from "readline-sync"
 //entrada
 const numero_inteiro = Number.parseInt(question('digite um número inteiro[Três dígitos]: '))
 //processamento
-var numero_inteiro_invertido = String(numero_inteiro).split('')
-numero_inteiro_invertido.reverse()
-numero_inteiro_invertido = Number(numero_inteiro_invertido.join(''))
+const centena = Number.parseInt(numero_inteiro / 100)
+const resto = numero_inteiro % 100
+const dezena = Number.parseInt(resto / 10)
+const unidade = resto % 10
+const numero_inteiro_invertido = unidade * 100 + dezena * 10 + centena
 const somatoria_numeros = numero_inteiro + numero_inteiro_invertido
 //saída
 console.log(`A soma entre ${numero_inteiro} e ${numero_inteiro_invertido} é igual a: ${somatoria_numeros}.`)
